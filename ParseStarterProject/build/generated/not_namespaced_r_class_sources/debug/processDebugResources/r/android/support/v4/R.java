bacca7315b5f9dded436ae5e529db0a5a8673016
brendan milton <mirrorimage101@hotmail.com>
2018-12-02 15:49:04 +0000

    Added user_list_activity that retrieves list of users from parse

--- ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
+++ ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
@@ -12,15 +12,15 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int font = 0x7f03009a;
-        public static final int fontProviderAuthority = 0x7f03009c;
-        public static final int fontProviderCerts = 0x7f03009d;
-        public static final int fontProviderFetchStrategy = 0x7f03009e;
-        public static final int fontProviderFetchTimeout = 0x7f03009f;
-        public static final int fontProviderPackage = 0x7f0300a0;
-        public static final int fontProviderQuery = 0x7f0300a1;
-        public static final int fontStyle = 0x7f0300a2;
-        public static final int fontWeight = 0x7f0300a3;
+        public static final int font = 0x7f0300a6;
+        public static final int fontProviderAuthority = 0x7f0300a8;
+        public static final int fontProviderCerts = 0x7f0300a9;
+        public static final int fontProviderFetchStrategy = 0x7f0300aa;
+        public static final int fontProviderFetchTimeout = 0x7f0300ab;
+        public static final int fontProviderPackage = 0x7f0300ac;
+        public static final int fontProviderQuery = 0x7f0300ad;
+        public static final int fontStyle = 0x7f0300ae;
+        public static final int fontWeight = 0x7f0300af;
     }
     public static final class bool {
         private bool() {}
@@ -46,21 +46,21 @@ public final class R {
         public static final int compat_button_padding_horizontal_material = 0x7f060065;
         public static final int compat_button_padding_vertical_material = 0x7f060066;
         public static final int compat_control_corner_material = 0x7f060067;
-        public static final int notification_action_icon_size = 0x7f060072;
-        public static final int notification_action_text_size = 0x7f060073;
-        public static final int notification_big_circle_margin = 0x7f060074;
-        public static final int notification_content_margin_start = 0x7f060075;
-        public static final int notification_large_icon_height = 0x7f060076;
-        public static final int notification_large_icon_width = 0x7f060077;
-        public static final int notification_main_column_padding_top = 0x7f060078;
-        public static final int notification_media_narrow_margin = 0x7f060079;
-        public static final int notification_right_icon_size = 0x7f06007a;
-        public static final int notification_right_side_padding_top = 0x7f06007b;
-        public static final int notification_small_icon_background_padding = 0x7f06007c;
-        public static final int notification_small_icon_size_as_large = 0x7f06007d;
-        public static final int notification_subtext_size = 0x7f06007e;
-        public static final int notification_top_pad = 0x7f06007f;
-        public static final int notification_top_pad_large_text = 0x7f060080;
+        public static final int notification_action_icon_size = 0x7f060078;
+        public static final int notification_action_text_size = 0x7f060079;
+        public static final int notification_big_circle_margin = 0x7f06007a;
+        public static final int notification_content_margin_start = 0x7f06007b;
+        public static final int notification_large_icon_height = 0x7f06007c;
+        public static final int notification_large_icon_width = 0x7f06007d;
+        public static final int notification_main_column_padding_top = 0x7f06007e;
+        public static final int notification_media_narrow_margin = 0x7f06007f;
+        public static final int notification_right_icon_size = 0x7f060080;
+        public static final int notification_right_side_padding_top = 0x7f060081;
+        public static final int notification_small_icon_background_padding = 0x7f060082;
+        public static final int notification_small_icon_size_as_large = 0x7f060083;
+        public static final int notification_subtext_size = 0x7f060084;
+        public static final int notification_top_pad = 0x7f060085;
+        public static final int notification_top_pad_large_text = 0x7f060086;
     }
     public static final class drawable {
         private drawable() {}
@@ -88,29 +88,29 @@ public final class R {
         public static final int action_text = 0x7f080017;
         public static final int actions = 0x7f080018;
         public static final int async = 0x7f080024;
-        public static final int blocking = 0x7f08002b;
-        public static final int cancel_action = 0x7f08003a;
-        public static final int chronometer = 0x7f08004a;
-        public static final int end_padder = 0x7f080062;
-        public static final int forever = 0x7f080066;
-        public static final int icon = 0x7f080071;
-        public static final int icon_group = 0x7f080072;
-        public static final int info = 0x7f08007a;
-        public static final int italic = 0x7f080081;
-        public static final int line1 = 0x7f080085;
-        public static final int line3 = 0x7f080086;
-        public static final int media_actions = 0x7f08008f;
-        public static final int normal = 0x7f08009a;
-        public static final int notification_background = 0x7f08009b;
-        public static final int notification_main_column = 0x7f08009c;
-        public static final int notification_main_column_container = 0x7f08009d;
-        public static final int right_icon = 0x7f0800b2;
-        public static final int right_side = 0x7f0800b3;
-        public static final int status_bar_latest_event_content = 0x7f0800d7;
-        public static final int text = 0x7f0800e3;
-        public static final int text2 = 0x7f0800e5;
-        public static final int time = 0x7f0800ec;
-        public static final int title = 0x7f0800ed;
+        public static final int blocking = 0x7f08002d;
+        public static final int cancel_action = 0x7f08003c;
+        public static final int chronometer = 0x7f08004d;
+        public static final int end_padder = 0x7f080067;
+        public static final int forever = 0x7f08006b;
+        public static final int icon = 0x7f080078;
+        public static final int icon_group = 0x7f080079;
+        public static final int info = 0x7f080080;
+        public static final int italic = 0x7f080088;
+        public static final int line1 = 0x7f08008d;
+        public static final int line3 = 0x7f08008e;
+        public static final int media_actions = 0x7f080099;
+        public static final int normal = 0x7f0800a4;
+        public static final int notification_background = 0x7f0800a5;
+        public static final int notification_main_column = 0x7f0800a6;
+        public static final int notification_main_column_container = 0x7f0800a7;
+        public static final int right_icon = 0x7f0800c0;
+        public static final int right_side = 0x7f0800c1;
+        public static final int status_bar_latest_event_content = 0x7f0800e8;
+        public static final int text = 0x7f0800f4;
+        public static final int text2 = 0x7f0800f6;
+        public static final int time = 0x7f0800fd;
+        public static final int title = 0x7f0800fe;
     }
     public static final class integer {
         private integer() {}
@@ -121,21 +121,21 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0a0025;
-        public static final int notification_action_tombstone = 0x7f0a0026;
-        public static final int notification_media_action = 0x7f0a0027;
-        public static final int notification_media_cancel_action = 0x7f0a0028;
-        public static final int notification_template_big_media = 0x7f0a0029;
-        public static final int notification_template_big_media_custom = 0x7f0a002a;
-        public static final int notification_template_big_media_narrow = 0x7f0a002b;
-        public static final int notification_template_big_media_narrow_custom = 0x7f0a002c;
-        public static final int notification_template_custom_big = 0x7f0a002d;
-        public static final int notification_template_icon_group = 0x7f0a002e;
-        public static final int notification_template_lines_media = 0x7f0a002f;
-        public static final int notification_template_media = 0x7f0a0030;
-        public static final int notification_template_media_custom = 0x7f0a0031;
-        public static final int notification_template_part_chronometer = 0x7f0a0032;
-        public static final int notification_template_part_time = 0x7f0a0033;
+        public static final int notification_action = 0x7f0a0026;
+        public static final int notification_action_tombstone = 0x7f0a0027;
+        public static final int notification_media_action = 0x7f0a0028;
+        public static final int notification_media_cancel_action = 0x7f0a0029;
+        public static final int notification_template_big_media = 0x7f0a002a;
+        public static final int notification_template_big_media_custom = 0x7f0a002b;
+        public static final int notification_template_big_media_narrow = 0x7f0a002c;
+        public static final int notification_template_big_media_narrow_custom = 0x7f0a002d;
+        public static final int notification_template_custom_big = 0x7f0a002e;
+        public static final int notification_template_icon_group = 0x7f0a002f;
+        public static final int notification_template_lines_media = 0x7f0a0030;
+        public static final int notification_template_media = 0x7f0a0031;
+        public static final int notification_template_media_custom = 0x7f0a0032;
+        public static final int notification_template_part_chronometer = 0x7f0a0033;
+        public static final int notification_template_part_time = 0x7f0a0034;
     }
     public static final class string {
         private string() {}
@@ -161,14 +161,14 @@ public final class R {
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] FontFamily = { 0x7f03009c, 0x7f03009d, 0x7f03009e, 0x7f03009f, 0x7f0300a0, 0x7f0300a1 };
+        public static final int[] FontFamily = { 0x7f0300a8, 0x7f0300a9, 0x7f0300aa, 0x7f0300ab, 0x7f0300ac, 0x7f0300ad };
         public static final int FontFamily_fontProviderAuthority = 0;
         public static final int FontFamily_fontProviderCerts = 1;
         public static final int FontFamily_fontProviderFetchStrategy = 2;
         public static final int FontFamily_fontProviderFetchTimeout = 3;
         public static final int FontFamily_fontProviderPackage = 4;
         public static final int FontFamily_fontProviderQuery = 5;
-        public static final int[] FontFamilyFont = { 0x7f03009a, 0x7f0300a2, 0x7f0300a3 };
+        public static final int[] FontFamilyFont = { 0x7f0300a6, 0x7f0300ae, 0x7f0300af };
         public static final int FontFamilyFont_font = 0;
         public static final int FontFamilyFont_fontStyle = 1;
         public static final int FontFamilyFont_fontWeight = 2;
--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,9 +11,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,13 +38,32 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener  {
 
     // Sign up or login
     Boolean signUpModeActive = true;
     TextView loginTextView;
 
-  @Override
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    // Acess activity_user_list.xml
+    public void showUserList(){
+        Intent intent = new Intent(getApplicationContext(), UserListActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+            signUpClicked(v);
+        }
+
+        return false;
+    }
+
+    @Override
   public void onClick(View v) {
     if(v.getId() == R.id.loginTextView){
 
@@ -55,15 +78,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             signUpButton.setText("Sign Up");
             loginTextView.setText("or, Login");
         }
+        // USER FRIENDLY KEYBOARD EDITION
+        // User clicks outside password or user id field (Logo or Layout)
+    } else if (v.getId() == R.id.logoImageView || v.getId() == R.id.backgroundLayout){
+        // Cast method return
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
     }
   }
 
   // Respond to Sign up OnClick
   public void signUpClicked(View view){
-
-    // Access edit text field variables
-    EditText usernameEditText = findViewById(R.id.usernameEditText);
-    EditText passwordEditText = findViewById(R.id.passwordEditText);
 
     // Check if user has entered text into both fields otherwise prompt the user
     if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")){
@@ -83,6 +108,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
               if(e == null){
                 //Ok
                 Log.i("Signup", "Success");
+                showUserList();
               } else {
                 // Log short error message to user
                 Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -96,6 +122,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 public void done(ParseUser user, ParseException e) {
                     if(user != null){
                         Log.i("Login", "Ok!");
+                        showUserList();
                     } else {
                         Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
@@ -113,6 +140,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     loginTextView = findViewById(R.id.loginTextView);
     loginTextView.setOnClickListener(this);
+
+      // Access edit text field variables
+      usernameEditText = findViewById(R.id.usernameEditText);
+      passwordEditText = findViewById(R.id.passwordEditText);
+
+      passwordEditText.setOnKeyListener(this);
+      ImageView logoImageView = findViewById(R.id.logoImageView);
+      RelativeLayout backgroundLayout =findViewById(R.id.backgroundLayout);
+
+      // Allow clicking whitespace and logo to move keyboard
+      logoImageView.setOnClickListener(this);
+      backgroundLayout.setOnClickListener(this);
+
+      // If user logged in show user list
+      if(ParseUser.getCurrentUser() != null){
+          showUserList();
+      }
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
