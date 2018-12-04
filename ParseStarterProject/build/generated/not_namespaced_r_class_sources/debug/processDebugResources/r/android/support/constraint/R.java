811667a52f2bcfdb1afcc70cba6d50861fab6078
brendan milton <mirrorimage101@hotmail.com>
2018-12-04 21:43:09 +0000

    Added ablity to upload images and to parse.

--- ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -82,11 +82,11 @@ public final class R {
         public static final int parent = 0x7f0800ab;
         public static final int percent = 0x7f0800ae;
         public static final int right = 0x7f0800bf;
-        public static final int spread = 0x7f0800e0;
-        public static final int spread_inside = 0x7f0800e1;
-        public static final int start = 0x7f0800e6;
-        public static final int top = 0x7f080104;
-        public static final int wrap = 0x7f08010d;
+        public static final int spread = 0x7f0800e1;
+        public static final int spread_inside = 0x7f0800e2;
+        public static final int start = 0x7f0800e7;
+        public static final int top = 0x7f080105;
+        public static final int wrap = 0x7f08010e;
     }
     public static final class styleable {
         private styleable() {}
